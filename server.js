313bcd3659fb02b35dde666a65aba12c35fc8e2e
Reth Ramos <rethanielramos@gmail.com>
2020-02-23 13:59:27 +0800

    changed project structure

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var port = 3000;
 var express = require('express');
 var app = express();
 
-app.use('/', express.static(__dirname + '/html', { index: 'login.html' }));
+app.use('/', express.static(__dirname, { index: 'login.html' }));
 app.use('/assets', express.static(__dirname + '/assets'));
 app.use('/scripts', express.static(__dirname + '/scripts'));
 
