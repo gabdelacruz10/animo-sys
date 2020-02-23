36d12741f5c2da0f0df7a681f909548ad9612def
Reth Ramos <rethanielramos@gmail.com>
2020-02-23 22:50:10 +0800

    modified style

--- scripts/styling-script.js
+++ scripts/styling-script.js
@@ -3,7 +3,7 @@ var list = document.getElementById('navlist').children;
 console.log(list);
 
 for (let i = 0; i < list.length; i++) {
-    var file = document.location.href.match(/[^\/]+$/)[0];
+    var file = document.location.href.match(/[^\/]+$/)[0] || 'index.html';
     if (file == list[i].firstElementChild.getAttribute('href')) {
         console.log(list[i].firstElementChild.getAttribute('href'));
         list[i].firstElementChild.id = 'active';
