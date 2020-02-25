c4a3e2cf322075a1707af4d647158f8755ebca9e
Reth Ramos <rethanielramos@gmail.com>
2020-02-25 11:09:12 +0800

    added font-size classes in styles.css
    If setting size, use the res- classes followed by the desired size

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,4 +1,4 @@
-function validate() {
+function validateStudentLogin() {
     var idNumber = document.getElementById("idNumber");
     var password = document.getElementById("password");
     var errMsg = document.getElementById('errMsg');
@@ -23,4 +23,31 @@ function validate() {
     }
 
 
+}
+
+function validateModLogin() {
+    var username = document.getElementById("username");
+    var password = document.getElementById("password");
+    var errMsg = document.getElementById('errMsg');
+    var bg = '#F09A97';
+    var err = 0;
+
+    // Validating user details should be done in server-side
+
+    if (username.value == '') {
+        err++;
+    }
+
+    if (password.value == '') {
+        err++;
+    }
+
+    if (err == 0) {
+        window.location.href = '/index.html';
+    } else {
+        username.style.borderColor = bg;
+        password.style.borderColor = bg;
+        errMsg.style.color = bg;
+        errMsg.innerHTML = "Invalid Username/Password.";
+    }
 }
