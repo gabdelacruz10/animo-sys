15d6f1e93b63955eed9e3b4415b2951afd670dbe
Reth Ramos <rethanielramos@gmail.com>
2020-03-01 21:03:15 +0800

    removed debug logs

--- scripts/table-scripts.js
+++ scripts/table-scripts.js
@@ -1,7 +1,6 @@
 var rows = document.getElementsByTagName('tr');
 
 for (let i = 1; i < rows.length; i++) {
-    console.log(rows[i].children);
     var row = rows[i];
     var cell = row.insertCell(-1);
     var button = document.createElement('button');
@@ -17,8 +16,6 @@ $("button").click(function () {
     if (input = confirm('Are you sure? All records of the student will be permanently deleted.')) {
         $(this).closest('tr').remove();
     }
-    console.log(input);
-
 });
 
 
